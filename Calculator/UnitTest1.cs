6e7891eac9f59b1050661168214a35a967dfb72c
Siladitya Das <83537766+siladityadas@users.noreply.github.com>
2025-04-10 17:41:14 +0200

    CodetestSum

--- Calculator/UnitTest1.cs
+++ Calculator/UnitTest1.cs
@@ -1,10 +1,16 @@
-﻿namespace Calculator;
+﻿using System;
+namespace Calculator;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-        Console.WriteLine("Mo maikina sundar");
+        if(sum(2,2)!=4)
+            throw new Exception();
+    }
+    int sum(int num1 , int num2)
+    {
+        return num1+num2;
     }
 }
